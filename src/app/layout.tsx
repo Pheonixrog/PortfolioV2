e3be3a74d410dafae35875fa35b608555bb3fab4
Rishabh Katiyar <rishabhasus9@gmail.com>
2024-09-05 14:32:20 +0530

    fixed navbar error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar darkMode={true} setDarkMode={true}  />
+        <Navbar   />
 
         {children}
       </body>
